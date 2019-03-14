312935df7ddaded6bc1bbeadd7f05c3ecf443e30
janngit <jannethgit@gmail.com>
2019-03-13 19:18:02 -0500

    Eliminada dependencia de import-export y código basura

--- web/fourth_umpire/admin.py
+++ web/fourth_umpire/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 #from .models import Match
-from import_export.admin import ImportExportModelAdmin
+
 from .models import *
 
-#admin.site.register(Match)
-@admin.register(DataSet, Match)
-class ViewAdmin(ImportExportModelAdmin):
-    exclude = ('id', )
+admin.site.register(Match)
